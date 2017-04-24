663fe0ec9755979e8802c93099540beffb39ff58
john sander <john sander>
2017-04-24 13:07:17 -0400

    logging updates

--- liri.js
+++ liri.js
@@ -31,6 +31,7 @@ for (var i = 3; i < nodeArgs.length; i++) {
 }
 
 if (process.argv[2] === "my-tweets") {
+    logEntry();
     myTweets();
 }
 
@@ -80,14 +81,13 @@ if (process.argv[2] === "do-what-it-says") {
 }
 
 function myTweets() {
-    logEntry();
     var params = {screen_name: 'jsander112'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
             for (var i = 0; i < tweets.length; i++) {
                 console.log(tweets[i].created_at)
                 console.log(tweets[i].text);
-                fs.appendFile("log.txt", "--------" + tweets[i].created_at + ", " + tweets[i].text, function() {});
+                fs.appendFile("log.txt", "----" + tweets[i].created_at + ", " + tweets[i].text, function() {});
             }
         }
     });
@@ -108,7 +108,7 @@ function spotifySongs() {
             fs.appendFile("log.txt", "The artist is " + data.tracks.items[i].artists[0].name + ", " +
             "The name of the song is " + data.tracks.items[i].name + ", " +
             "The preview url is " + data.tracks.items[i].preview_url + ", " +
-            "The name of the album is " + data.tracks.items[i].album.name + "---------", function() {});
+            "The name of the album is " + data.tracks.items[i].album.name + "----", function() {});
             }
     });
 }
@@ -126,7 +126,7 @@ function defaultSong() {
             fs.appendFile("log.txt", " The artist is " + data.tracks.items[0].artists[0].name + ", " +
             "The name of the song is " + data.tracks.items[0].name + ", " +
             "The preview url is " + data.tracks.items[0].preview_url + ", " +
-            "The name of the album is " + data.tracks.items[0].album.name + "---------", function() {});
+            "The name of the album is " + data.tracks.items[0].album.name, function() {});
     });
 }
 
@@ -135,7 +135,7 @@ function movieThis() {
     request(queryUrl, function(error, response, body) {
         if (!error && response.statusCode === 200) {
             // console.log(JSON.parse(body));
-            console.log("Move Title: " + JSON.parse(body).Title);
+            console.log("Movie Title: " + JSON.parse(body).Title);
             console.log("Year the movie came out: " + JSON.parse(body).Year);
             console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
             console.log("Country where the movie was produced: " + JSON.parse(body).Country);
@@ -144,12 +144,12 @@ function movieThis() {
             console.log("Actors in movie: " + JSON.parse(body).Actors);
             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
             console.log("Website:" + JSON.parse(body).Website);
-            fs.appendFile("log.txt", "--------Move Title: " + JSON.parse(body).Title + ", " +
+            fs.appendFile("log.txt", "Move Title: " + JSON.parse(body).Title + ", " +
             "Year the movie came out: " + JSON.parse(body).Year + ", " + "IMDB Rating: " + JSON.parse(body).imdbRating +
             ", " + "Country where the movie was produced: " + JSON.parse(body).Country + ", " +
             "Movie language: " + JSON.parse(body).Language + ", " + "Movie Plot: " + JSON.parse(body).Plot +
             ", " + "Actors in movie: " + JSON.parse(body).Actors + ", " + "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value +
-            ", " + "Website:" + JSON.parse(body).Website, function() {});
+            ", " + "Website: " + JSON.parse(body).Website, function() {});
         }
     });
 }
@@ -168,7 +168,7 @@ function defaultMovie() {
             console.log("Actors in movie: " + JSON.parse(body).Actors);
             console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
             console.log("Website: " + JSON.parse(body).Website);
-            fs.appendFile("log.txt", "--------Move Title: " + JSON.parse(body).Title + ", " +
+            fs.appendFile("log.txt", "Movie Title: " + JSON.parse(body).Title + ", " +
             "Year the movie came out: " + JSON.parse(body).Year + ", " + "IMDB Rating: " + JSON.parse(body).imdbRating +
             ", " + "Country where the movie was produced: " + JSON.parse(body).Country + ", " +
             "Movie language: " + JSON.parse(body).Language + ", " + "Movie Plot: " + JSON.parse(body).Plot +
@@ -179,6 +179,6 @@ function defaultMovie() {
 }
 
 function logEntry() {
-    fs.appendFile("log.txt", "*****" + process.argv[2] + ", " + songSearch, function() {});
+    fs.appendFile("log.txt", "********" + process.argv[2] + ", " + songSearch + "--------", function() {});
 }
 
